fb0003b149033d79a257e60ed7f9efffd65276c6
ninaCydeo <ninap9@icloud.com>
2023-09-15 09:41:34 -0400

    Task num1 -Odd or Even By Nina

--- src/week01/OddEvenNp.java
+++ src/week01/OddEvenNp.java
@@ -1,4 +1,14 @@
 package week01;
 
 public class OddEvenNp {
+
 }
+/*
+Task01:
+Number -- odd & even
+Write a method which identifies if given number is even or odd
+EX:
+identify(5) --> "Odd"
+identify(6) --> "Even"
+
+ */
