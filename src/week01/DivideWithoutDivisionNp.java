8f327a5eaebdd5ac0eadc02fbe6381659d920178
ninaCydeo <ninap9@icloud.com>
2023-09-15 18:57:59 -0400

    Divide without division task uploaded.

--- src/week01/DivideWithoutDivisionNp.java
+++ src/week01/DivideWithoutDivisionNp.java
@@ -2,6 +2,15 @@ package week01;
 
 public class DivideWithoutDivisionNp {
 
+    public static void DivideWithoutOperathor(int x,int y){
+        int count=0;
 
+        while (x>=y){
+            x=x-y;
+
+            count++;
+        }
+        System.out.println(count);
+    }
 
 }
