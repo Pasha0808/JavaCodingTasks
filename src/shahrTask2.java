1f7302bf626ba73bbc1e2de68466aff88649106d
Shahrzad wardak <shahrzadwardak143@gmail.com>
2023-09-17 10:07:24 -0400

    task num2 added

---
+++ src/shahrTask2.java
@@ -0,0 +1,20 @@
+public class shahrTask2 {
+
+    public static void main(String[] args) {
+        /*
+
+    public static void divide (int x, int y) {
+        int num =x;
+        int count =0;
+
+        for (int i= x; num>=y; i++ ){
+            num=num-y;
+            count++;
+        }
+        System.out.println("Number "+x+" divided by "+y+ " = "+count+" with a remainder of "+x%y);
+
+         */
+
+
+    }
+}
