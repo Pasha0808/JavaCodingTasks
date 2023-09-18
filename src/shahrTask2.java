3b77bd87939c5a6c21d7e954fc77bc6f88953d9f
Shahrzad wardak <shahrzadwardak143@gmail.com>
2023-09-18 15:01:36 -0400

    added task2

--- src/shahrTask2.java
+++ src/shahrTask2.java
@@ -8,7 +8,7 @@ Numbers -- Divide without / operator
 Write a method that can divide two numbers without using division operator
 
          */
-//added
+//added task2
 
         public static void main(String[] args) {
         divideWithoutDivision(7,4);
