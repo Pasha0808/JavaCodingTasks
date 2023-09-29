e815e501e9ebc5f9e618e561df6d7a0519bf018b
ninaCydeo <ninap9@icloud.com>
2023-09-29 16:07:51 -0400

    SwapNumbers another change

--- src/week01/SwapNumbersNp.java
+++ src/week01/SwapNumbersNp.java
@@ -17,8 +17,10 @@ public class SwapNumbersNp {
     a=a-b;
     //a=4;
 
-        System.out.println("a = " + a);
         System.out.println("b = " + b);
+        System.out.println("a = " + a);
+
+
 
 
 
