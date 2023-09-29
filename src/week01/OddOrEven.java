a29ab8fe85c712a8e0106526815e889a2e5a46c1
faycal <faycalsfaya@gmail.com>
2023-09-29 09:33:22 -0400

    Odd or even task

---
+++ src/week01/OddOrEven.java
@@ -0,0 +1,15 @@
+package week01;
+
+public class OddOrEven {
+
+    public static void oddOrEven(int x){
+
+        if ( x % 2 == 0 ){
+            System.out.println(x+ " is even");
+        }
+        if( x % 2 != 0){
+            System.out.println(x+ " is odd");
+        }
+
+    }
+}
