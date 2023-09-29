7f14e44ea09312f979e03d9dba297e9874fad597
ninaCydeo <ninap9@icloud.com>
2023-09-29 15:54:45 -0400

    Task num1 -Odd or Even By Nina

--- src/week01/SwapNumbersNp.java
+++ src/week01/SwapNumbersNp.java
@@ -2,4 +2,26 @@ package week01;
 
 public class SwapNumbersNp {
 
+    //swap two variables values without using third variable
+
+
+    public static void main(String[] args) {
+
+    int a=10;
+    int b=4;
+
+    a=a+b; //14
+    //a=14;
+    b=a-b; //b=10;
+
+    a=a-b;
+    //a=4;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+    }
+
 }
