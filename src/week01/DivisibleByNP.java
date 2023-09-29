d15b834ed0c7430a33ac63cb59c581455c93b161
ninaCydeo <ninap9@icloud.com>
2023-09-29 15:21:29 -0400

    Task num1 -Odd or Even By Nina

---
+++ src/week01/DivisibleByNP.java
@@ -0,0 +1,30 @@
+package week01;
+
+public class DivisibleByNP {
+
+    public static void main(String[] args) {
+
+//divisible by 3
+
+        for (int i = 1; i < 100; i++) {
+            if(i%3==0){
+                System.out.println(i + ",");
+            }
+        }
+
+//divisible by 5
+
+
+        for (int i = 1; i <100 ; i++) {
+            if(i%5==0){
+                System.out.println(i + ",");
+            }
+
+        }
+
+//number divisible by 15
+
+
+
+    }
+}
