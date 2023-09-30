1752051e74f732f59656e5e56937b7f0b3ebc7d9
Oksana <ovaysman@gmail.com>
2023-09-30 08:45:24 -0400

    week two Oksana's tasks

---
+++ src/week_02/divisible_by_3_5_15.java
@@ -0,0 +1,25 @@
+package week_02;
+
+public class divisible_by_3_5_15 {
+
+    public static void main(String[] args) {
+        String divisibleBy15 = "";
+        String divisibleBy5 = "";
+        String divisibleBy3 = "";
+
+        for (int i = 1; i <+100 ; i++) {
+            if (i%3==0&&i%5==0&&i%15==0){
+                divisibleBy15= divisibleBy15 +  i +" ";
+            }
+            if (i%3==0&&i%15!=0){
+                divisibleBy3= divisibleBy3 +  i +" ";
+            }
+            if (i%5==0&&i%15!=0){
+                divisibleBy5= divisibleBy5 +  i +" ";
+            }
+        }
+        System.out.println("divisible by 15: "+divisibleBy15);
+        System.out.println("divisible by 5: " +divisibleBy5);
+        System.out.println("divisible by 3: "+divisibleBy3);
+    }
+}
---
+++ src/week_02/swap_two_numbers.java
@@ -0,0 +1,25 @@
+package week_02;
+
+public class swap_two_numbers {
+
+    public static void main(String[] args) {
+
+
+        int  a = 10;
+        int b = 5;
+
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
+
+      a=a+b;
+      b=a-b;
+      a=a-b;
+
+
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
+
+    }
+
+    }
+
