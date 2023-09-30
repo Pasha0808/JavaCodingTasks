a2ab33ad39974205e2984629d6e2f579559fd967
Pasha0808 <pavloromanyuk@yahoo.com>
2023-09-29 23:21:23 -0400

    second task update final

--- src/week02/week02_Pavlo.java
+++ src/week02/week02_Pavlo.java
@@ -41,6 +41,31 @@ public class week02_Pavlo {
 
         System.out.println("--------------------------------------------------");
 
+        for (int i=1;i<=24;i++){
+
+            if (i%2==0 && i%3==0 &&i%5==0) {
+                System.out.println("Codility Test Coders");
+
+            } else if (i%2==0 && i%5==0) {
+                System.out.println("Codility Coders");
+
+            }else if (i%2==0 && i%3==0){
+                System.out.println("Codility Test");
+
+            }else if (i%2==0){
+                System.out.println("Codility");
+
+            }else if (i%3==0){
+                System.out.println("Test");
+
+            } else if (i%5==0) {
+                System.out.println("Coders");
+
+            }else {
+                System.out.println(i);
+            }
+        }
+
 
 
 
