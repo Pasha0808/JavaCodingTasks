8f718b30b24ea5c2d7e33fbd07170eda5766f761
zahideren <erozgur80@hotmail.com>
2023-10-19 11:03:04 -0400

    update  swapping number class

--- src/week02/SwapingNumbersZTE.java
+++ src/week02/SwapingNumbersZTE.java
@@ -3,6 +3,14 @@ package week02;
 public class SwapingNumbersZTE {
     public static void main(String[] args) {
 
-//add something
+        int a=10;
+        int b=5;
+
+        a =a+b;
+        b = a - b;
+        a = a -b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
     }
 }
