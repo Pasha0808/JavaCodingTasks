0fa5530b6242afa3cd98b5410ff4b4f93d457a2b
zahideren <erozgur80@hotmail.com>
2023-10-26 10:24:53 -0400

    update swapping number task

--- src/week02/SwapingNumbersZTE.java
+++ src/week02/SwapingNumbersZTE.java
@@ -3,6 +3,21 @@ package week02;
 public class SwapingNumbersZTE {
     public static void main(String[] args) {
 
-//add something
+        /*
+        number         swapping number
+        a=10             a=5;
+        b=5              b=10;
+         */
+
+        int a = 10;
+        int b = 5;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
     }
 }
